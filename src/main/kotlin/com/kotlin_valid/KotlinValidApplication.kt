32fd331833f058121c976891b9a42d530a25ec33
Gyunny <wjdrbs966@naver.com>
2021-10-12 15:51:29 +0900

    initial commit

---
+++ src/main/kotlin/com/kotlin_valid/KotlinValidApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin_valid
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinValidApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinValidApplication>(*args)
+}
---
+++ src/test/kotlin/com/kotlin_valid/KotlinValidApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin_valid
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinValidApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
